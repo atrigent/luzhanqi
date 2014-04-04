61569a2699c50547885cecba9f90e63fa697bb90
Ari G. Entlich <atrigent@ccs.neu.edu>
2014-04-03 22:41:02 -0400

    Store a value in Movement which specifies whether the move went around a railroad corner.

--- luzhanqi.py
+++ luzhanqi.py
@@ -42,6 +42,9 @@ class Movement:
     - attack: an AttackInfo if this is an attack move
     - move_type: the strings INITIAL, ROAD, or RAILROAD,
         depending on how the move was made
+    - railroad_corner: None if move_type is not RAILROAD,
+        and True or False if this move went around a
+        railroad corner
     """
 
     def __init__(self, board, piece, end, outcome=None):
@@ -87,6 +90,14 @@ class Movement:
                 raise ValueError('This is not an attack!')
 
             self.attack = None
+
+        self.railroad_corner = None
+        if self.move_type == 'RAILROAD':
+            lines = LuzhanqiBoard.nonabsolute_railroad_lines()
+
+            self.railroad_corner = all(not match_sequence(self.end, line)
+                                       for line in lines
+                                       if match_sequence(self.start, line))
 
 class BoardPiece:
     """Represents a piece on the board with a type and an event history.
